f7912b17ee5f9dd7781a1c7860f369ef65862cf8
Caleb Yung <phyung0107@gmail.com>
2022-08-15 00:25:19 +0800

    debug

--- src/signal_extraction.py
+++ src/signal_extraction.py
@@ -216,9 +216,9 @@ class SignalExtraction:
 
     def gen_signal_10k_all_stocks(self):
         # generate signal per CIK
-        pool = multiprocessing.Pool(processes=self.config['n_jobs'])
-        feats = pool.map(self.gen_signal_10k, self.master_idx_10k.cik.unique().tolist())
-        # feats = [self.gen_signal_10k(x) for x in self.master_idx_10k.cik.unique().tolist()]
+        # pool = multiprocessing.Pool(processes=self.config['n_jobs'])
+        # feats = pool.map(self.gen_signal_10k, self.master_idx_10k.cik.unique().tolist())
+        feats = [self.gen_signal_10k(x) for x in self.master_idx_10k.cik.unique().tolist()]
         feats = pd.concat([x for x in feats if x!=None]).sort_values('doc_id').reset_index(drop=True)
 
         # map back to stock
@@ -309,9 +309,9 @@ class SignalExtraction:
 
     def gen_signal_10q_all_stocks(self):
         # generate signal per CIK
-        pool = multiprocessing.Pool(processes=self.config['n_jobs'])
-        feats = pool.map(self.gen_signal_10q, self.master_idx_10q.cik.unique().tolist())
-        # feats = [self.gen_signal_10q(x) for x in self.master_idx_10q.cik.unique().tolist()]
+        # pool = multiprocessing.Pool(processes=self.config['n_jobs'])
+        # feats = pool.map(self.gen_signal_10q, self.master_idx_10q.cik.unique().tolist())
+        feats = [self.gen_signal_10q(x) for x in self.master_idx_10q.cik.unique().tolist()]
         feats = pd.concat([x for x in feats if x!=None]).sort_values('doc_id').reset_index(drop=True)
 
         # map back to stock
