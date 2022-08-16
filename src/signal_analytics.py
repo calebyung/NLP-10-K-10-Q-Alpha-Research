44cc9c7c36f352bda2b112f009a9ed8a6467abc1
Caleb Yung <phyung0107@gmail.com>
2022-08-16 20:25:14 +0800

    debug

--- src/signal_analytics.py
+++ src/signal_analytics.py
@@ -65,17 +65,17 @@ class SignalAnalytics:
         log(f'Shape of 8-K feats: {feats_8k.shape}')
 
         # load LTR signal
-        # feats_lgbm_ltr = load_pkl(f'../input/hkml-lightgbm-ltr/pred_val_out').rename(columns={0:'feat_lgbm_ltr_12m'})
-        # log(f'Shape of LGBM Learning-to-Rank feats: {feats_lgbm_ltr.shape}')
+        feats_lgbm_ltr = load_pkl(os.path.join(const.INTERIM_DATA_PATH, 'pred_val_out.pkl')).rename(columns={0:'feat_lgbm_ltr_12m'})
+        log(f'Shape of LGBM Learning-to-Rank feats: {feats_lgbm_ltr.shape}')
 
         # combine all signals into single df
-            # .merge(feats_lgbm_ltr, how='outer', on=['stock','date']) \
         self.feats = feats_10k \
             .merge(feats_10q, how='outer', on=['stock','date']) \
             .merge(feats_8k, how='outer', on=['stock','date']) \
+            .merge(feats_lgbm_ltr, how='outer', on=['stock','date']) \
             .sort_values(['stock','date']) \
             .groupby('stock') \
-            .apply(lambda x: x.ffill()) \
+            .apply(lambda x: x.ffill().filna(0)) \
             .loc[lambda x: (x.date>=self.config['bt_start_date']) & (x.date<=self.config['bt_end_date'])] \
             .reset_index(drop=True)
 
